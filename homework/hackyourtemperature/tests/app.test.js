b7ff0fb6ef4c44ddfabe251c1b261f01469ad97a
Osamah Qassem <ohm.qassem@gmail.com>
2023-01-25 12:09:49 +0100

    tests

--- homework/hackyourtemperature/tests/app.test.js
+++ homework/hackyourtemperature/tests/app.test.js
@@ -1,13 +1,27 @@
 import supertest from "supertest";
 import app from '../app.js';
 
-describe ('post /cityName',()=>
-{
+const request = supertest(app);
 
-})
-
-describe( " when the name is in correct",()=>{
-
-
-    
-})
+describe('POST /weather', () => {
+    it('should have city name ', async () => {
+      const response = await request
+        .post('/weather')
+        .send({ cityName: 'Amsterdam' });
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toContain('Amsterdam');
+    });
+  
+    it('there is no cityName ', async () => {
+      const response = await request.post('/weather');
+      expect(response.statusCode).toBe(400);
+      expect(response.text).toContain('City is not found!');
+    });
+  
+    it('city name is gibberish', async () => {
+      const response = await request.post('/weather').send({ cityName: 'adaad' });
+  
+      expect(response.statusCode).toBe(404);
+      expect(response.text).toContain('CityName is not found!');
+    });
+  });
